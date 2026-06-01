022e1c3475f86386fdaf517e97abf304296ca66f
charanlakkam331 <charanlakkam0309@gmail.com>
2026-06-01 15:04:37 +0530

    logout api added

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from apps.core.views.auth import SendOTPAPIView, VerifyOTPAPIView
+from apps.core.views.auth import SendOTPAPIView, VerifyOTPAPIView, LogoutAPIView
 
 urlpatterns = [
 
     path("send-otp", SendOTPAPIView.as_view(), name="send-otp"),
 
     path("verify-otp", VerifyOTPAPIView.as_view(), name="verify-otp"),
+
+    path("logout", LogoutAPIView.as_view(), name="logout"),
 
 ]
