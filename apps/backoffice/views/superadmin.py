9abf6dcd88c3336bf019c4bdc133c050e77be776
charanlakkam331 <charanlakkam0309@gmail.com>
2026-06-04 09:53:57 +0530

    lead edit api added

--- apps/backoffice/views/superadmin.py
+++ apps/backoffice/views/superadmin.py
@@ -15,7 +15,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.views import APIView
 from rest_framework import status
 
-
+from django.shortcuts import get_object_or_404
 
 class CreateSuperAdminAPIView(APIView):
 
