bdb8be07a14a84911ca4990a1bace9e074cb21d8
charanlakkam331 <charanlakkam0309@gmail.com>
2026-06-16 19:31:53 +0530

    middleware changes

--- shared/permissions/rbac.py
+++ shared/permissions/rbac.py
@@ -1,8 +1,9 @@
 from rest_framework.permissions import BasePermission
 
+from apps.school.models import School
 from shared.helpers.rbac import (
     get_user_roles,
-    has_permission,
+    has_permission, get_user_permissions,
 )
 
 
@@ -27,9 +28,46 @@ class HasRole(BasePermission):
         )
 
 
+# class HasPermission(BasePermission):
+#
+#     def has_permission(self, request, view):
+#
+#         required_permission = getattr(
+#             view,
+#             "required_permission",
+#             None,
+#         )
+#
+#         if not required_permission:
+#             return True
+#
+#         return has_permission(
+#             request.user,
+#             required_permission,
+#         )
+
 class HasPermission(BasePermission):
 
     def has_permission(self, request, view):
+
+        school_id = request.headers.get("X-School-Id")
+
+        request.current_school = None
+
+        if school_id:
+            request.current_school = School.objects.filter(
+                id=school_id,
+            ).first()
+
+        request.roles = get_user_roles(
+            user=request.user,
+            school_id=school_id,
+        )
+
+        request.permissions = get_user_permissions(
+            user=request.user,
+            school_id=school_id,
+        )
 
         required_permission = getattr(
             view,
@@ -40,7 +78,4 @@ class HasPermission(BasePermission):
         if not required_permission:
             return True
 
-        return has_permission(
-            request.user,
-            required_permission,
-        )
+        return required_permission in request.permissions
