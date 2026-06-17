ee7d9451a8a7173836f776c40364c9edc1ef8cac
charanlakkam331 <charanlakkam0309@gmail.com>
2026-06-17 12:06:13 +0530

    middleware changes

--- shared/permissions/rbac.py
+++ shared/permissions/rbac.py
@@ -28,62 +28,9 @@ class HasRole(BasePermission):
         )
 
 
-# class HasPermission(BasePermission):
-#
-#     def has_permission(self, request, view):
-#
-#         required_permission = getattr(
-#             view,
-#             "required_permission",
-#             None,
-#         )
-#
-#         if not required_permission:
-#             return True
-#
-#         return has_permission(
-#             request.user,
-#             required_permission,
-#         )
-
 class HasPermission(BasePermission):
 
     def has_permission(self, request, view):
-
-        print("=" * 80)
-        print("HasPermission Called")
-        print("User :", request.user)
-        print("Is Authenticated :", request.user.is_authenticated)
-
-        school_id = request.headers.get(
-            "X-School-Id",
-        )
-
-        print("X-School-Id :", school_id)
-
-        request.current_school = None
-
-        if school_id:
-
-            request.current_school = School.objects.filter(
-                id=school_id,
-            ).first()
-
-        print("Current School :", request.current_school)
-
-        request.roles = get_user_roles(
-            user=request.user,
-            school_id=school_id,
-        )
-
-        print("Roles :", request.roles)
-
-        request.permissions = get_user_permissions(
-            user=request.user,
-            school_id=school_id,
-        )
-
-        print("Permissions :", request.permissions)
 
         required_permission = getattr(
             view,
@@ -91,21 +38,74 @@ class HasPermission(BasePermission):
             None,
         )
 
-        print("Required Permission :", required_permission)
-
         if not required_permission:
-
-            print("No Permission Required")
-            print("=" * 80)
-
             return True
 
-        has_access = (
-            required_permission
-            in request.permissions
+        return has_permission(
+            request.user,
+            required_permission,
         )
 
-        print("Has Access :", has_access)
-        print("=" * 80)
-
-        return has_access
+# class HasPermission(BasePermission):
+#
+#     def has_permission(self, request, view):
+#
+#         print("=" * 80)
+#         print("HasPermission Called")
+#         print("User :", request.user)
+#         print("Is Authenticated :", request.user.is_authenticated)
+#
+#         school_id = request.headers.get(
+#             "X-School-Id",
+#         )
+#
+#         print("X-School-Id :", school_id)
+#
+#         request.current_school = None
+#
+#         if school_id:
+#
+#             request.current_school = School.objects.filter(
+#                 id=school_id,
+#             ).first()
+#
+#         print("Current School :", request.current_school)
+#
+#         request.roles = get_user_roles(
+#             user=request.user,
+#             school_id=school_id,
+#         )
+#
+#         print("Roles :", request.roles)
+#
+#         request.permissions = get_user_permissions(
+#             user=request.user,
+#             school_id=school_id,
+#         )
+#
+#         print("Permissions :", request.permissions)
+#
+#         required_permission = getattr(
+#             view,
+#             "required_permission",
+#             None,
+#         )
+#
+#         print("Required Permission :", required_permission)
+#
+#         if not required_permission:
+#
+#             print("No Permission Required")
+#             print("=" * 80)
+#
+#             return True
+#
+#         has_access = (
+#             required_permission
+#             in request.permissions
+#         )
+#
+#         print("Has Access :", has_access)
+#         print("=" * 80)
+#
+#         return has_access
