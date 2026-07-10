9f3cea4ce595cf3fd705aedc84eecc4d1ff602f9
charanlakkam331 <charanlakkam0309@gmail.com>
2026-07-10 10:58:52 +0530

    ptm api's added

--- apps/ptm/urls.py
+++ apps/ptm/urls.py
@@ -4,5 +4,5 @@ from apps.ptm.views.ptm import StudentPTMListAPIView, StudentPTMResponseAPIView
 
 urlpatterns = [
     path("parent-teacher-meetings",StudentPTMListAPIView.as_view()),
-    path("parent-response",StudentPTMResponseAPIView.as_view()),
+    path("parent-response/<uuid:meeting_id>",StudentPTMResponseAPIView.as_view()),
 ]
