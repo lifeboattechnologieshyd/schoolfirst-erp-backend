71169b698969c3741e3ad78f8a272bb751a3e76e
charanlakkam331 <charanlakkam0309@gmail.com>
2026-07-17 11:27:34 +0530

    changes in  student ptm list api

--- apps/ptm/views/ptm.py
+++ apps/ptm/views/ptm.py
@@ -204,6 +204,7 @@ class StudentPTMListAPIView(APIView):
 
             return CustomResponse.successResponse(
                 description="Parent teacher meetings fetched successfully.",
+                total_count=len(data),
                 data={
                     "student": {
                         "id": str(student.id),
