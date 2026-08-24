a710f1f01671206a7ac8793043a24ade4eb0b98f
charanlakkam331 <charanlakkam0309@gmail.com>
2026-08-24 12:44:43 +0530

    sections api response changes

--- apps/homework/views/assignment.py
+++ apps/homework/views/assignment.py
@@ -1,5 +1,4 @@
-from datetime import timezone
-
+from django.utils import timezone
 from django.db import transaction
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
